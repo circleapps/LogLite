2543fbc601dd5a11a6d7406741227a6506f49fcf
liaowei <edwardweiliao@gmail.com>
2023-01-22 21:20:33 +0800

    refine dialog

--- src/LogDialog.js
+++ src/LogDialog.js
@@ -1,22 +1,18 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
-import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import Box from '@mui/material/Box';
 import { TextField } from '@mui/material';
 
 import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { bottom } from '@popperjs/core';
+
 
 export default function LogDialog(props) {
     const { onClose, selectedValue, open, log } = props;
@@ -32,27 +28,20 @@ export default function LogDialog(props) {
     return (
         <Dialog onClose={handleClose} open={open}>
             <DialogTitle>View Log</DialogTitle>
-            <Stack spacing={2}
-                sx = {{
-                }}
-                 >
-                <Item>
-                    <TextField id="txtId" label="Id" variant="standard" value={log.id} />
-                </Item>
-                <Item>
-                    <TextField label="Time" variant="standard" value={log.timestamp} />
-                </Item>
-                <Item>
-                    <TextField label="Category" variant="standard" value={log.category} />
-                </Item>
-                <Item>
-                    <TextField fullWidth={true} label="Message" variant="standard" multiline={true} minRows={30} rows={30} value={log.message} 
-                     sx = {{
-                        width: 600
-                    }}
-                    />
-                </Item>
-            </Stack>
+            <DialogContent>
+                <TextField label="ID" variant="standard" value={"#" + log.id} 
+                    sx = {{ width: 550, paddingBottom:3 }}
+                 />
+                <TextField label="Time" variant="standard" value={log.timestamp} 
+                    sx = {{ width: 550, paddingBottom:3 }}
+                />
+                <TextField label="Category" variant="standard" value={log.category} 
+                    sx = {{ width: 550, paddingBottom:3 }}
+                />
+                <TextField label="Message" variant="standard" multiline={true} value={log.message} minRows={32}
+                    sx = {{ width: 550, paddingBottom:3 }}
+                />
+            </DialogContent>
         </Dialog>
     );
 }
