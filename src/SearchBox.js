38d445c3c0686bbc76f19bc009cd94954dc41976
liaowei <edwardweiliao@gmail.com>
2023-01-22 10:39:57 +0800

    added date picker

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,15 +3,27 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-export default class SearchBox extends React.Component
-{
-    render() {
-        return (
-            <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off" >
-                <TextField id="txtCategory" label="Category" variant="standard" />
-                <TextField id="txtMessage" label="Message" variant="standard" />
-                <Button variant="contained">Search</Button>
-            </Box>
-        )
-    }
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+export default function SearchBox() {
+    const [value, setValue] = React.useState(null);
+    return (
+        <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off" >
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                    label="Date"
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                </LocalizationProvider>
+            <TextField id="txtCategory" label="Category" variant="standard" />
+            <TextField id="txtMessage" label="Message" variant="standard" />
+            <Button variant="contained">Search</Button>
+        </Box>
+    )
 }
